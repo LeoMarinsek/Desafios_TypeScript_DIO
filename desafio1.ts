e61794309ce32225d873a22d20a4d46506de3fca
LeoMarinsek <leonardomarinsek@hotmail.com>
2022-06-12 23:19:50 -0300

    Resolução de desafios propostos pela aula de TypeScript da DIO

---
+++ desafio1.ts
@@ -0,0 +1,21 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+let funcionario = {
+    codigo: 10,
+    nome: 'Jhon'
+};
+
+//Respostas:
+let funcionario2: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: "Jhon"
+}
+
+interface Funcionario {
+    codigo: number,
+    nome: string
+}
+
+let funcionario3: Funcionario = {
+    codigo: 200,
+    nome: 'Jhon'
+}
